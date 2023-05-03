048e2801f37e18d32f0d8a7b3a786edcdee0cd1e
James <jallerton00@gmail.com>
2023-05-03 23:02:45 +0100

    Initial working voltage

--- software/esphome/powersensor.h
+++ software/esphome/powersensor.h
@@ -1,5 +1,5 @@
 #include "esphome.h"
-#include "EmonLib/EmonLib.h"
+#include "EmonLib.h"
 
 class MyPowerSensor : public PollingComponent, public Sensor {
  public:
@@ -7,11 +7,13 @@ class MyPowerSensor : public PollingComponent, public Sensor {
   Sensor *realpower_sensor = new Sensor();
   Sensor *apparentpower_sensor = new Sensor();
   Sensor *powerfactor_sensor = new Sensor();
-  MyPowerSensor() : PollingComponent(1000) { }
+  Sensor *voltage_sensor = new Sensor();
+  Sensor *current_sensor = new Sensor();
+  MyPowerSensor() : PollingComponent(500) { }
   
   void setup() override {
-   emon1.voltage(34, 269.5, 1.7);
-   emon1.current(35, 4.9);
+   emon1.voltage(33, 520, 1.5);
+   emon1.current(35, 0.1);
   }
   
   void update() override {
@@ -22,5 +24,7 @@ class MyPowerSensor : public PollingComponent, public Sensor {
    apparentpower_sensor->publish_state(apparentPower);
    float powerFactor= emon1.powerFactor;
    powerfactor_sensor->publish_state(powerFactor);
+   voltage_sensor->publish_state(emon1.Vrms);
+   current_sensor->publish_state(emon1.Irms);
    }
 };
