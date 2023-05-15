069132fa8569c7d40f80a5fd67925d6e3562279c
Jallerton00 <33671033+Jallerton00@users.noreply.github.com>
2023-05-15 21:24:04 +0100

    Add device classes and manage sensor units from custom integration (rather than YAML)

--- software/esphome/powersensor.h
+++ software/esphome/powersensor.h
@@ -1,6 +1,12 @@
 #include "esphome.h"
 #include "EmonLib.h"
 
+#define VOLTAGE_PIN 33
+#define VOLTAGE_CALIBRATION 57.19
+#define PHASE_CALIBRATION 1.28
+#define CURRENT_1_CALIBRATION 26.4
+#define CURRENT_1_PIN 34
+
 class MyPowerSensor : public PollingComponent, public Sensor {
  public:
   EnergyMonitor emon1;
@@ -9,11 +15,31 @@ class MyPowerSensor : public PollingComponent, public Sensor {
   Sensor *powerfactor_sensor = new Sensor();
   Sensor *voltage_sensor = new Sensor();
   Sensor *current_sensor = new Sensor();
-  MyPowerSensor() : PollingComponent(1000) { }
+  MyPowerSensor() : PollingComponent(2000) { }
   
   void setup() override {
-   emon1.voltage(33, 54, 1.7);
-   emon1.current(35, 1);
+   voltage_sensor->set_unit_of_measurement("V");
+   voltage_sensor->set_accuracy_decimals(2);
+   voltage_sensor->set_device_class("voltage");
+
+   current_sensor->set_unit_of_measurement("A");
+   current_sensor->set_accuracy_decimals(2);
+   current_sensor->set_device_class("current");
+
+   realpower_sensor->set_unit_of_measurement("W");
+   realpower_sensor->set_accuracy_decimals(2);
+   realpower_sensor->set_device_class("power");
+
+   apparentpower_sensor->set_unit_of_measurement("VA");
+   apparentpower_sensor->set_accuracy_decimals(2);
+   apparentpower_sensor->set_device_class("power");
+
+   powerfactor_sensor->set_accuracy_decimals(2);
+   powerfactor_sensor->set_device_class("power");
+
+
+   emon1.voltage(VOLTAGE_PIN, VOLTAGE_CALIBRATION, PHASE_CALIBRATION);
+   emon1.current(CURRENT_1_PIN, CURRENT_1_CALIBRATION);
   }
   
   void update() override {
